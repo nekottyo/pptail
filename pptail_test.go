711890760d4ca572148afd3fa1f76693d6fdcfa4
nekottyo <nekottyo@gmail.com>
2019-04-10 12:15:38 +0900

    Add test

--- pptail_test.go
+++ pptail_test.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestPrint(t *testing.T) {
+func TestPrint_syslogFormat(t *testing.T) {
 	cases := []struct {
 		in  string
 		out message
@@ -21,7 +20,7 @@ func TestPrint(t *testing.T) {
 			},
 		},
 		{
-			`Apr 09 15:57:50 localhost 33ad0cd34e88[947]: 1970-01-01 00:33:39.000000347 +0000 some/image:latest.service_image_1.91880377002d.json: {WORNG JSON}`,
+			`Apr 09 15:57:50 localhost 33ad0cd34e88[947]: 1970-01-01 00:33:39.000000347 +0000 some/image:latest.service_image_1.91880377002d.json: {INVALID JSON}`,
 			message{
 				Date:    "Apr 09 15:57:50",
 				Image:   "some/image:latest.service_image_1.91880377002d",
@@ -33,7 +32,40 @@ func TestPrint(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.in, func(t *testing.T) {
 			actual := parse(c.in, syslogFormat)
-			fmt.Printf("actual = %#v\n", actual)
+
+			if diff := cmp.Diff(actual, c.out); diff != "" {
+				t.Errorf("parse func differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestPrint_fluentdFormat(t *testing.T) {
+	cases := []struct {
+		in  string
+		out message
+	}{
+		{
+			`2019-04-02T08:03:47+09:00       some/image:latest.service_image.3cba43465250  {"level":"INFO", "message":"test"}`,
+			message{
+				Date:    "2019-04-02T08:03:47+09:00",
+				Image:   "some/image:latest.service_image.3cba43465250",
+				Payload: map[string]interface{}{"level": "INFO", "message": "test"},
+			},
+		},
+		{
+			`2019-04-02T08:03:47+09:00       some/image:latest.service_image.3cba43465250  {INVALID JSON}`,
+			message{
+				Date:    "2019-04-02T08:03:47+09:00",
+				Image:   "some/image:latest.service_image.3cba43465250",
+				Payload: nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.in, func(t *testing.T) {
+			actual := parse(c.in, tdAgentFormat)
 
 			if diff := cmp.Diff(actual, c.out); diff != "" {
 				t.Errorf("parse func differs: (-got +want)\n%s", diff)
